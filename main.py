921fab5379cc1398a88b4f7a7953c883ee4b5f90
JimWasHere <98727234+JimWasHere@users.noreply.github.com>
2022-05-28 13:01:46 -0500

    made routing automatic

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import inspect
+
 import justpy as jp
 
 from webapp.about import About
 from webapp.dictionary import Dictionary
 from webapp.home import Home
+from webapp import page
 
-jp.Route(Home.path, Home.serve)
-jp.Route(About.path, About.serve)
-jp.Route(Dictionary.path, Dictionary.serve)
+imports = list(globals().values())
+
+for x in imports:
+    if inspect.isclass(x):
+        if issubclass(x, page.Page) and x is not page.Page:
+            jp.Route(x.path, x.serve)
+
+
+# jp.Route(Home.path, Home.serve)
+# jp.Route(About.path, About.serve)
+# jp.Route(Dictionary.path, Dictionary.serve)
+
 jp.justpy(port=8001)
---
+++ webapp/page.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+
+class Page:
+
+    @abstractmethod
+    def serve(self):
+        pass
